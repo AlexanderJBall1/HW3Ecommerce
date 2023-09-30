8c2d78e28258a48af28670547844b949dec89573
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-09-30 13:15:46 -0500

    Create Players.php

---
+++ Players.php
@@ -0,0 +1,9 @@
+<?php
+require_once("util-db.php")
+require_once("model-players.php")
+$pageTitle = "Players";
+include "view-header.php";
+$players = selectPlayers();
+include "view-players.php"
+include "view-footer.php"
+?>
