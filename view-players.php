418d3c20cd518617ab3874efe7367027641f6f15
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-09-30 14:31:40 -0500

    Create view-players.php

---
+++ view-players.php
@@ -0,0 +1,16 @@
+<h1>Players</h1>
+<div class="table-responsive">
+  <table class="table">
+    <thead>
+      <th>ID</th>
+      <th>Name</th>
+      <th>Number</th>
+      <th>Team</th>
+    </thead>
+    <tbody>
+<?php
+while ($player = 
+?>
+    </tbody>
+  </table>
+</div>
