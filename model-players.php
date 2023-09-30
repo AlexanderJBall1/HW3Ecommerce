bda6343ebb2a00a8a1ef7a494e126dc3e9732710
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-09-30 13:23:14 -0500

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -1,10 +1,8 @@
 <?php
-function selectAccountsByOwner($uEmail) {
+function selectPlayers($uEmail) {
     try {
         $conn = get_db_connection();
-        $stmt = $conn->prepare("select a.account_id, account_name, o.active, ifnull((select count(account_user_id) from account_user where account_id=a.account_id),0) as users 
-            from account a join account_owner o on o.account_id=a.account_id where owner_email=? order by account_name");
-        $stmt->bind_param("s", $uEmail);
+        $stmt = $conn->prepare("SELECT Player_ID, Player_Name, Player_Number, Player_Team FROM `Player`");
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
