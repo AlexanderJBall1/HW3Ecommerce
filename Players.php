33c3c93d75370be64de6464ffdb9581e2a64bfc8
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-03 17:03:07 -0500

    Update Players.php

--- Players.php
+++ Players.php
@@ -4,6 +4,6 @@ require_once("model-players.php")
 $pageTitle = "Players";
 include "view-header.php";
 $players = selectPlayers();
-include "view-players.php"
-include "view-footer.php"
+include "view-players.php";
+include "view-footer.php";
 ?>
