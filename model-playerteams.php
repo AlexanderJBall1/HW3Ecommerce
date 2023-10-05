bec29968338bc66b9c7c5eb0443cd089a0386750
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-04 20:46:41 -0500

    Create model-playerteams.php

---
+++ model-playerteams.php
@@ -0,0 +1,15 @@
+<?php
+function selectPlayerTeams() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Player_ID, Player_Name, Player_Number, Player_Team FROM `Player`");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
