58c4ed3b98235c2060a689e0ac9729c3164f7610
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-04 21:27:16 -0500

    Update Player-Teams.php

--- Player-Teams.php
+++ Player-Teams.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-playerteams.php");
 $pageTitle = "Player Teams";
 include "view-header.php";
-$players = selectPlayerTeams($_GET['id']);
+$players = selectPlayerTeams($_POST['ptid']);
 include "view-playerteams.php";
 include "view-footer.php";
 ?>
