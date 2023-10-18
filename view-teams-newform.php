b188455d1922792ba7c7aef7df22ef053cc11312
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-18 14:04:40 -0500

    Update view-teams-newform.php

--- view-teams-newform.php
+++ view-teams-newform.php
@@ -7,19 +7,25 @@
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+<div class="modal fade" id="newTeamModal" tabindex="-1" aria-labelledby="newTeamModalLabel" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="newTeamModalLabel">New Team</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+<div class="modal-body">
+<form method = "post" action="">
+  <div class="mb-3">
+    <label for="tName" class="form-label">Team Name</label>
+    <input type="text" class="form-control" id="tName" name = "tName">
+  </div>
+  <div class="mb-3">
+    <label for="tCity" class="form-label">Team City</label>
+    <input type="text" class="form-control" id="tCity" name = "tCity">
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
       </div>
     </div>
   </div>
