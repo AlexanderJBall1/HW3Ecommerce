d2e4e5accb4e5e1635ae92fb7eede38cf8103ad7
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-19 16:49:20 -0500

    Update coaches-by-teams.php

--- coaches-by-teams.php
+++ coaches-by-teams.php
@@ -1,1 +1,9 @@
-
+<?php
+require_once("util-db.php");
+require_once("model-coaches-by-teams.php");
+$pageTitle = "Team Coaches";
+include "view-header.php";
+$coaches = selectCoachesByTeams($_POST['cid']);
+include "view-coaches-by-teams.php";
+include "view-footer.php";
+?>
