992832f6425f0072be8f75285516b34a8835692b
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-19 13:17:22 -0500

    Update model-players.php

--- model-players.php
+++ model-players.php
@@ -1,8 +1,9 @@
 <?php
-function selectPlayers() {
+function selectPlayers($pid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT Player_ID, Player_Name, Player_Number, Player_Team FROM `Player`");
+        $stmt->bind_param("I", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
