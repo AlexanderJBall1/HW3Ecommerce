95b34bcb1d5ac04125d0d9b3fa6c9feb71db5a7e
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-19 13:11:36 -0500

    Update model-playerteams.php

--- model-playerteams.php
+++ model-playerteams.php
@@ -3,7 +3,7 @@ function selectPlayerTeams($pid) {
     try {
         $conn = get_db_connection();
         $stmt = $conn->prepare("SELECT p.Player_ID, Player_Name, Player_Number, Player_Team, Salary, Years FROM `Player` p JOIN PlayerTeam pt ON p.Player_ID = pt.Player_ID where pt.Team_ID = ?");
-        $stmt->bind_param("p", $pid);
+        $stmt->bind_param("i", $pid);
         $stmt->execute();
         $result = $stmt->get_result();
         $conn->close();
