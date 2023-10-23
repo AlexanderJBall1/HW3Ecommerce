5f839c1a0dafe608a63f0ab4315b859412067ab0
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-23 14:08:26 -0500

    Update Coaches.php

--- Coaches.php
+++ Coaches.php
@@ -3,7 +3,35 @@ require_once("util-db.php");
 require_once("model-coaches.php");
 $pageTitle = "Coaches";
 include "view-header.php";
-$Coaches = selectCoaches();
+
+if (isset($_POST['actionType'])){
+  switch($_POST['actionType']){
+    case "Add":
+      if(insertTeam($_POST['cName'], $_POST['cRecord'])){
+        echo '<div class="alert alert-success" role="alert">Team Added</div>';
+      } else{
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+
+    case "Edit":
+      if(updateTeam($_POST['cName'], $_POST['cRecord'], $_POST['cid'])){
+        echo '<div class="alert alert-success" role="alert">Team Edited.</div>';
+      } else{
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+
+    case "Delete":
+      if(deleteTeam($_POST['cid'],)){
+        echo '<div class="alert alert-success" role="alert">Team Deleted.</div>';
+      } else{
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+  }
+}
+$teams = selectCoaches();
 include "view-coaches.php";
 include "view-footer.php";
 ?>
