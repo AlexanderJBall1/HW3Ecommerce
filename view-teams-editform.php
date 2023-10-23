327b11a08043df560ed402a2f09b08ff0a0b29ad
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-23 09:35:03 -0500

    Update view-teams-editform.php

--- view-teams-editform.php
+++ view-teams-editform.php
@@ -1,27 +1,28 @@
 <!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editTeamModal<?php echo $team['Team_ID']; ?">
+<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#editTeamModal<?php echo $team['Team_ID']; ?>"
 
 </button>
 
 <!-- Modal -->
-<div class="modal fade" id="newTeamModal" tabindex="-1" aria-labelledby="newTeamModalLabel" aria-hidden="true">
+<div class="modal fade" id="editTeamModal<?php echo $team['Team_ID']; ?">" tabindex="-1" aria-labelledby="editTeamModalLabel<?php echo $team['Team_ID']; ?>" aria-hidden="true">
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="newTeamModalLabel">New Team</h1>
+        <h1 class="modal-title fs-5" id="editTeamModalLabel<?php echo $team['Team_ID']; ?>">Edit Team</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
 <div class="modal-body">
 <form method = "post" action="">
   <div class="mb-3">
-    <label for="tName" class="form-label">Team Name</label>
-    <input type="text" class="form-control" id="tName" name = "tName">
+    <label for="tName<?php echo $team['Team_ID']; ?>" class="form-label">Team Name</label>
+    <input type="text" class="form-control" id="tName<?php echo $team['Team_ID']; ?>" name = "tName" value = "<?php echo $team['Name']; ?>">
   </div>
   <div class="mb-3">
-    <label for="tCity" class="form-label">Team City</label>
-    <input type="text" class="form-control" id="tCity" name = "tCity">
+    <label for="tCity<?php echo $team['Team_ID']; ?>" class="form-label">Team City</label>
+    <input type="text" class="form-control" id="tCity<?php echo $team['Team_ID']; ?>" name = "tCity" value = "<?php echo $team['City']; ?>" >
   </div>
-        <input type = "hidden" name = "actionType" value = "Add">
+        <input type = "hidden" name = "tid" value = "<?php echo $team['Team_ID']; ?>">
+        <input type = "hidden" name = "actionType" value = "Edit">
   <button type="submit" class="btn btn-primary">Save</button>
 </form>
       </div>
