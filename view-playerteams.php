26198a2329f71669d6133783ece927c24e20fa99
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-23 13:32:07 -0500

    Update view-playerteams.php

--- view-playerteams.php
+++ view-playerteams.php
@@ -3,6 +3,7 @@
   <table class="table">
     <thead>
       <th>ID</th>
+      <th>Name</th>
       <th>Salary</th>
       <th>Years</th>
     </thead>
@@ -12,6 +13,7 @@ while ($player = $players->fetch_assoc()){
 ?>
   <tr>
     <td><?php echo $player['Player_ID']; ?></td>
+    <td><?php echo $player['Player_Name']; ?></td>
     <td><?php echo $player['Salary']; ?></td>
     <td><?php echo $player['Years']; ?></td>
   </tr>
