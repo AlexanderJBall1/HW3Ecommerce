01d913bee59150ec993e3fafcf50fd5fe1dc9e94
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-23 20:49:21 -0500

    Update model-playerteams.php

--- model-playerteams.php
+++ model-playerteams.php
@@ -1,24 +1,5 @@
 <?php
 
-<?php
-function selectPlayers() {
-    try {
-        $conn = get_db_connection();
-        $stmt = $conn->prepare("SELECT Player_ID, Player_Name, Player_Number, Player_Team FROM `Player`");
-        $stmt->execute();
-        $result = $stmt->get_result();
-        $conn->close();
-        return $result;
-    } catch (Exception $e) {
-        $conn->close();
-        throw $e;
-    }
-}
-?>
-
-
-
-
 
 function selectPlayerTeams($tid) {
     try {
