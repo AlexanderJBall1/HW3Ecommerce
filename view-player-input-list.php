87560ce60f4a92b7561e1df86e29e0ef8bef0d5c
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-25 12:28:46 -0500

    Update view-player-input-list.php

--- view-player-input-list.php
+++ view-player-input-list.php
@@ -1,9 +1,14 @@
 <select class="form-select" id="tid" name="tid">
 <?php
 while ($teamItem = $teamList->fetch_assoc()) {
-  <option value="<?php echo $teamItem['Team_ID']; ?>"<?=$selText?>><?php echo $teamItem['Name']; ?></option>
-?>
-<?php
+    $selText = "";
+  if ($selectedTeam == $teamItem['Team_ID']) {
+    $selText = " selected";
+  }
 }
+?>
+<option value="<?php echo $teamItem['Team_ID']; ?>"<?=$selText?>><?php echo $teamItem['Name']; ?></option>
+<?php
+  }
 ?>
 </select>
