bb56312dca9e28e890a3f10cb76b4d69b0a9d4f0
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-25 08:59:07 -0500

    Update players-on-teams.php

--- players-on-teams.php
+++ players-on-teams.php
@@ -3,7 +3,7 @@ require_once("util-db.php");
 require_once("model-players-on-teams.php");
 $pageTitle = "Players on Teams";
 include "view-header.php";
-$playersonteams = selectPlayerOnTeams($_GET['tid']);
+$teams = selectTeams();
 include "view-player-on-teams.php";
 include "view-footer.php";
 ?>
