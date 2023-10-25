74755c3e202c5bb67ad180b16821d6831f07b10a
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-25 15:48:46 -0500

    Update players-on-teams.php

--- players-on-teams.php
+++ players-on-teams.php
@@ -6,7 +6,7 @@ include "view-header.php";
 if (isset($_POST['actionType'])) {
   switch ($_POST['actionType']) {
     case "Add":
-      if (insertPlayerTeam($_POST['tid'], $_POST['pid'], $_POST['yrs'], $_POST['num'], $_POST['salary'])) {
+      if (insertPlayerTeam($_POST['tid'], $_POST['pid'], $_POST['ptSalary'], $_POST['num'], $_POST['ptYearsPlayed'])) {
         echo '<div class="alert alert-success" role="alert">Section added.</div>';
       } else {
         echo '<div class="alert alert-danger" role="alert">Error.</div>';
