04946ae0fab6413f53496722f641af78dca349bf
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-25 10:05:21 -0500

    Update model-players-on-teams.php

--- model-players-on-teams.php
+++ model-players-on-teams.php
@@ -28,4 +28,75 @@ function selectPlayerOnTeams($tid) {
         throw $e;
     }
 }
+
+function selectPlayersForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Player_ID, Player_Name FROM `Player` order by Player_Name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function selectTeamsForInput() {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("SELECT Team_ID, Team_Name FROM `Team` order by Team_Name");
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        return $result;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function insertPlayerTeam($tid, $pid, $yrs, $num, $salary) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("INSERT INTO `PlayerTeam` (`Team_ID`, `Player_ID`, `Years`, `Number`, `Salary`) VALUES (?, ?, ?, ?, ?)");
+        $stmt->bind_param("iisss", $tid, $pid, $yrs, $num, $salary);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function updatePlayerTeam($tid, $pid, $yrs, $num, $salary, $ptid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("update `PlayerTeam` set `Team_ID` = ?, `Player_ID` = ?, `Years` = ?, `Number` = ?, `Salary` = ? where PT_id = ?");
+        $stmt->bind_param("iisssi", $tid, $pid, $yrs, $num, $salary, $ptid)
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+
+function deletePlayerTeam($ptid) {
+    try {
+        $conn = get_db_connection();
+        $stmt = $conn->prepare("delete from PlayerTeam where PT_ID=?");
+        $stmt->bind_param("i", $ptid);
+        $success = $stmt->execute();
+        $conn->close();
+        return $success;
+    } catch (Exception $e) {
+        $conn->close();
+        throw $e;
+    }
+}
+?>
 ?>
