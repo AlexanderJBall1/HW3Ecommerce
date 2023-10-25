d132e12087caf1b318f1e01231d13d60f37d4e86
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-25 07:14:48 -0500

    Update view-player-on-teams.php

--- view-player-on-teams.php
+++ view-player-on-teams.php
@@ -5,19 +5,22 @@ while ($team = $teams->fetch_assoc()){
 ?>
 `<div class="card">
     <div class="card-body">
-      <h5 class="card-title">Card title</h5>
-      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-    </div>
-    <div class="card-footer">
-      <small class="text-body-secondary">Last updated 3 mins ago</small>
+      <h5 class="card-title"><?php echo $team['Name']; ?></h5>
+      <p class="card-text">
+        <ul class="list-group">
+<?php
+ $playeronteams = selectPlayerOnTeams($team['Team_ID']);
+while($player = $playeronteams->fetch_assoc()){
+?>
+    <li class="list-group-item"><?php echo $player['Player_Name']; ? - <?php echo $player['Salary']; ?> - <td><?php echo $player['Years']; ?></td></li>
+<?php
+}
+?>
+
+        </ul>
+      </p>
     </div>
   </div>
-  <tr>
-    <td><?php echo $player['Player_ID']; ?></td>
-    <td><?php echo $player['Player_Name']; ?></td>
-    <td><?php echo $player['Salary']; ?></td>
-    <td><?php echo $player['Years']; ?></td>
-  </tr>
 <?php
 }
 ?>
