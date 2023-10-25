c43917dea4d8d3361a2144c0deb666d6413d5fc3
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-25 06:55:43 -0500

    Update view-player-on-teams.php

--- view-player-on-teams.php
+++ view-player-on-teams.php
@@ -1,16 +1,17 @@
 <h1>Player Info</h1>
-<div class="table-responsive">
-  <table class="table">
-    <thead>
-      <th>ID</th>
-      <th>Name</th>
-      <th>Salary</th>
-      <th>Years</th>
-    </thead>
-    <tbody>
+<div class="card-group">
 <?php
-while ($player = $playeronteams->fetch_assoc()){
+while ($team = $teams->fetch_assoc()){
 ?>
+`<div class="card">
+    <div class="card-body">
+      <h5 class="card-title">Card title</h5>
+      <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+    </div>
+    <div class="card-footer">
+      <small class="text-body-secondary">Last updated 3 mins ago</small>
+    </div>
+  </div>
   <tr>
     <td><?php echo $player['Player_ID']; ?></td>
     <td><?php echo $player['Player_Name']; ?></td>
