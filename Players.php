337eb0ba984762cb53692b53afb29fe43b243023
AlexanderJBall1 <123108734+AlexanderJBall1@users.noreply.github.com>
2023-10-25 20:51:49 -0500

    Update Players.php

--- Players.php
+++ Players.php
@@ -3,6 +3,34 @@ require_once("util-db.php");
 require_once("model-players.php");
 $pageTitle = "Players";
 include "view-header.php";
+
+if (isset($_POST['actionType'])){
+  switch($_POST['actionType']){
+    case "Add":
+      if(insertTeam($_POST['tName'], $_POST['tCity'])){
+        echo '<div class="alert alert-success" role="alert">Team Added</div>';
+      } else{
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+
+    case "Edit":
+      if(updateTeam($_POST['tName'], $_POST['tCity'], $_POST['tid'])){
+        echo '<div class="alert alert-success" role="alert">Team Edited.</div>';
+      } else{
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+
+    case "Delete":
+      if(deleteTeam($_POST['tid'],)){
+        echo '<div class="alert alert-success" role="alert">Team Deleted.</div>';
+      } else{
+        echo '<div class="alert alert-danger" role="alert">Error</div>';
+      }
+      break;
+  }
+}
 $players = selectPlayers();
 include "view-players.php";
 include "view-footer.php";
